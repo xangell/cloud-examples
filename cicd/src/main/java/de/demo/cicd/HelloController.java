cd2a017cb31d283bf64cf4a748fc5eef2e5fd6a6
xangell <xangell@xangell-HP-Desktop-M01-F0xxx>
2021-05-07 06:29:14 +0200

    Added error case

--- cicd/src/main/java/de/demo/cicd/HelloController.java
+++ cicd/src/main/java/de/demo/cicd/HelloController.java
@@ -3,6 +3,7 @@ package de.demo.cicd;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import reactor.core.publisher.Mono;
 
@@ -13,6 +14,11 @@ public class HelloController {
 	@GetMapping("/sayhello")
 	public Mono<String> sayHello() {
 	    return Mono.just("Hello");
+	}
+	
+	@GetMapping("/error")
+	public Mono<String> generateError() {
+	    return Mono.error(new WebClientResponseException(403, "This did not work", null, null, null));
 	}
 
 }
